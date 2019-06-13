7942e5e213401cf838b593088c6e5db4be8716d0
Camille Baillat <camille.baillat@pm.me>
2019-06-13 15:55:03 +0200

    Add various extensions for Android

---
+++ src/main/java/me/camillebc/utilities/extensions/FragmentExtensions.kt
@@ -0,0 +1,5 @@
+package me.camillebc.utilities.extensions
+
+inline fun androidx.fragment.app.FragmentManager.inTransaction(func: androidx.fragment.app.FragmentTransaction.() -> androidx.fragment.app.FragmentTransaction) {
+    beginTransaction().func().commit()
+}
